3849391b707406aa7c38ed2285bd3e820312a861
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-08-03 16:24:12 +0300

    real time messaging completed with socket io

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,8 +8,13 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"]
   },
 });
+
+export const getReceiverSocketId = (receiverId)=>{
+    return userSocketMap[receiverId]
+}
 
 const userSocketMap = {};
 
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,9 +1,11 @@
 import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
 import MessageSkeleton from "../skeleton/MessageSkeleton";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
   const { loading, messages } = useGetMessages();
+  useListenMessages();
   return (
     <div className="px-4 flex-1 overflow-auto">
       {loading ? (

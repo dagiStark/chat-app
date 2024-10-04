1255b3705c2f949dca51f46a445f647d8e6c6bfd
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-04 23:23:24 +0300

    domain in production

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,13 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://chat-app-client-five-xi.vercel.app/",
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/auth", authRoutes);

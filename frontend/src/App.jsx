089a6a12d3bdf63fe85b8cbd17d147f0cc6e1d10
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-04 20:26:41 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,24 @@ import SignUp from "./pages/signup/SignUp";
 import Home from "./pages/home/Home";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthContext } from "./context/AuthContext";
-import "./App.css";
 
 function App() {
-  const {authUser} = useAuthContext();
+  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-center p-4 h-screen">
       <Routes>
-        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={authUser ? <Navigate to = "/"/> : <SignUp />} />
-        <Route path="/login" element={authUser ? <Navigate to = "/"/> : <Login />} />
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
       </Routes>
     </div>
   );

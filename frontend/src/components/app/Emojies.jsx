fbfce16e7bab4001f914d2439d8c0755728aeb19
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-11 14:48:15 +0300

    updated emoji list

--- frontend/src/components/app/Emojies.jsx
+++ frontend/src/components/app/Emojies.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { emojiList } from "../../constants";
 
-function Emojies() {
-  return <div>Emojies</div>;
-}
+const Emojies = () => {
+
+
+  return (
+    <div>
+      <h3>Emojies</h3>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          height: "650px", 
+          overflowY: "scroll", 
+          border: "1px solid #ccc", 
+          padding: "10px",
+        }}
+      >
+        {emojiList.map((emoji, index) => (
+          <span key={index} style={{ fontSize: "2rem", margin: "0px" }}>
+            {emoji}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Emojies;

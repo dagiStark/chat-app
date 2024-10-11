3921f7e810ba2d7b0ff0f27c00479c50f6dd4569
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-11 14:36:46 +0300

    added logic for rendering components

--- frontend/src/components/app/RightSideBar.jsx
+++ frontend/src/components/app/RightSideBar.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
+import Emojies from "./Emojies";
+import Stickers from "./Stickers";
+import Gifs from "./Gifs";
 
 function RightSideBar() {
+  const [selectedComponent, setSelectedComponent] = useState("Emojies");
+
+  const renderComponent = () => {
+    switch (selectedComponent) {
+      case "Emojies":
+        return <Emojies />;
+      case "Stickers":
+        return <Stickers />;
+      case "Gifs":
+        return <Gifs />;
+      default:
+        return <Emojies />;
+    }
+  };
+
   return (
-    <div>
-        right side bar
-      
+    <div className="flex flex-col w-full">
+      <div className="flex justify-between">
+        {/* Navbar */}
+        <button onClick={() => setSelectedComponent("Emojies")}>Emojies</button>
+        <button onClick={() => setSelectedComponent("Stickers")}>Stickers</button>
+        <button onClick={() => setSelectedComponent("Gifs")}>Gifs</button>
+      </div>
+      <div>
+        {/* Render Selected Component */}
+        {renderComponent()}
+      </div>
     </div>
-  )
+  );
 }
 
-export default RightSideBar
+export default RightSideBar;

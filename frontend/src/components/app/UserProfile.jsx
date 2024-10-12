0a7573cba2d68e214f842d64de2b35c24db50e4d
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-12 15:22:24 +0300

    ui update

--- frontend/src/components/app/UserProfile.jsx
+++ frontend/src/components/app/UserProfile.jsx
@@ -19,126 +19,129 @@ const UserProfile = () => {
         padding: "16px",
       }}
     >
-      {/* Profile Header */}
-      <Grid
-        container
-        justifyContent="center"
-        alignItems="center"
-        direction="column"
-      >
-        <Avatar
-          alt="Michael Wong"
-          src={selectedConversation?.profilePic}
-          sx={{ width: 80, height: 80 }}
-        />
-        <Typography variant="h6" component="h2" gutterBottom>
-          {selectedConversation?.fullName}
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          {`@${selectedConversation?.username}`}
-        </Typography>
-        {/* Social Icons */}
-        <Grid
-          container
-          justifyContent="center"
-          spacing={1}
-          sx={{ marginTop: "8px" }}
-        >
-          <Grid item="true">
-            <IconButton size="small" color="primary">
-              {/* Replace with actual icons for Facebook, Twitter, LinkedIn */}
-              <i className="fab fa-facebook"></i>
-            </IconButton>
-          </Grid>
-          <Grid item="true">
-            <IconButton size="small" color="primary">
-              <i className="fab fa-twitter"></i>
-            </IconButton>
-          </Grid>
-          <Grid item="true">
-            <IconButton size="small" color="primary">
-              <i className="fab fa-linkedin"></i>
-            </IconButton>
-          </Grid>
-        </Grid>
-      </Grid>
-
-      <Divider sx={{ margin: "16px 0" }} />
-
-      {/* Contact Info */}
-      <Grid container spacing={2}>
-        <Grid item="true" xs={12}>
-          <Typography variant="body2" color="textSecondary">
-            Mobile
-          </Typography>
-          <Grid container alignItems="center">
-            <PhoneIcon fontSize="small" />
-            <Typography variant="body1" sx={{ marginLeft: "8px" }}>
-              +430 332 4567
+      {selectedConversation && (
+        <>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            direction="column"
+          >
+            <Avatar
+              alt="Michael Wong"
+              src={selectedConversation?.profilePic}
+              sx={{ width: 80, height: 80 }}
+            />
+            <Typography variant="h6" component="h2" gutterBottom>
+              {selectedConversation?.fullName}
             </Typography>
-          </Grid>
-        </Grid>
-
-        <Grid item="true" xs={12}>
-          <Typography variant="body2" color="textSecondary">
-            Email
-          </Typography>
-          <Grid container alignItems="center">
-            <EmailIcon fontSize="small" />
-            <Typography variant="body1" sx={{ marginLeft: "8px" }}>
-              mizko@gmail.com
+            <Typography variant="body2" color="textSecondary">
+              {`@${selectedConversation?.username}`}
             </Typography>
+            {/* Social Icons */}
+            <Grid
+              container
+              justifyContent="center"
+              spacing={1}
+              sx={{ marginTop: "8px" }}
+            >
+              <Grid item="true">
+                <IconButton size="small" color="primary">
+                  {/* Replace with actual icons for Facebook, Twitter, LinkedIn */}
+                  <i className="fab fa-facebook"></i>
+                </IconButton>
+              </Grid>
+              <Grid item="true">
+                <IconButton size="small" color="primary">
+                  <i className="fab fa-twitter"></i>
+                </IconButton>
+              </Grid>
+              <Grid item="true">
+                <IconButton size="small" color="primary">
+                  <i className="fab fa-linkedin"></i>
+                </IconButton>
+              </Grid>
+            </Grid>
           </Grid>
-        </Grid>
 
-        <Grid item="true" xs={12}>
-          <Typography variant="body2" color="textSecondary">
-            Date of Birth
-          </Typography>
-          <Grid container alignItems="center">
-            <CalendarTodayIcon fontSize="small" />
-            <Typography variant="body1" sx={{ marginLeft: "8px" }}>
-              02/12/1990
+          <Divider sx={{ margin: "16px 0" }} />
+
+          {/* Contact Info */}
+          <Grid container spacing={2}>
+            <Grid item="true" xs={12}>
+              <Typography variant="body2" color="textSecondary">
+                Mobile
+              </Typography>
+              <Grid container alignItems="center">
+                <PhoneIcon fontSize="small" />
+                <Typography variant="body1" sx={{ marginLeft: "8px" }}>
+                  +430 332 4567
+                </Typography>
+              </Grid>
+            </Grid>
+
+            <Grid item="true" xs={12}>
+              <Typography variant="body2" color="textSecondary">
+                Email
+              </Typography>
+              <Grid container alignItems="center">
+                <EmailIcon fontSize="small" />
+                <Typography variant="body1" sx={{ marginLeft: "8px" }}>
+                  mizko@gmail.com
+                </Typography>
+              </Grid>
+            </Grid>
+
+            <Grid item="true" xs={12}>
+              <Typography variant="body2" color="textSecondary">
+                Date of Birth
+              </Typography>
+              <Grid container alignItems="center">
+                <CalendarTodayIcon fontSize="small" />
+                <Typography variant="body1" sx={{ marginLeft: "8px" }}>
+                  02/12/1990
+                </Typography>
+              </Grid>
+            </Grid>
+
+            <Grid item="true" xs={12}>
+              <Typography variant="body2" color="textSecondary">
+                Gender
+              </Typography>
+              <Grid container alignItems="center">
+                <MaleIcon fontSize="small" />
+                <Typography variant="body1" sx={{ marginLeft: "8px" }}>
+                  {selectedConversation?.gender}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Divider sx={{ margin: "16px 0" }} />
+
+          {/* Shared Media */}
+          <Grid container justifyContent="space-between" alignItems="center">
+            <Typography variant="body2" color="textSecondary">
+              Shared Media
             </Typography>
+            <Button variant="text" size="small">
+              See All
+            </Button>
           </Grid>
-        </Grid>
 
-        <Grid item="true" xs={12}>
-          <Typography variant="body2" color="textSecondary">
-            Gender
-          </Typography>
-          <Grid container alignItems="center">
-            <MaleIcon fontSize="small" />
-            <Typography variant="body1" sx={{ marginLeft: "8px" }}>
-              {selectedConversation?.gender}
-            </Typography>
+          <Grid container spacing={1} sx={{ marginTop: "8px" }}>
+            <Grid item="true" xs={4}>
+              <PlayCircleOutlineIcon fontSize="large" sx={{ width: "100%" }} />
+            </Grid>
+            <Grid item="true" xs={4}>
+              <PlayCircleOutlineIcon fontSize="large" sx={{ width: "100%" }} />
+            </Grid>
+            <Grid item="true" xs={4}>
+              <PlayCircleOutlineIcon fontSize="large" sx={{ width: "100%" }} />
+            </Grid>
           </Grid>
-        </Grid>
-      </Grid>
-
-      <Divider sx={{ margin: "16px 0" }} />
-
-      {/* Shared Media */}
-      <Grid container justifyContent="space-between" alignItems="center">
-        <Typography variant="body2" color="textSecondary">
-          Shared Media
-        </Typography>
-        <Button variant="text" size="small">
-          See All
-        </Button>
-      </Grid>
-
-      <Grid container spacing={1} sx={{ marginTop: "8px" }}>
-        <Grid item="true" xs={4}>
-          <PlayCircleOutlineIcon fontSize="large" sx={{ width: "100%" }} />
-        </Grid>
-        <Grid item="true" xs={4}>
-          <PlayCircleOutlineIcon fontSize="large" sx={{ width: "100%" }} />
-        </Grid>
-        <Grid item="true" xs={4}>
-          <PlayCircleOutlineIcon fontSize="large" sx={{ width: "100%" }} />
-        </Grid>
-      </Grid>
+        </>
+      )}
     </div>
   );
 };
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -51,7 +51,7 @@ export default MessageContainer;
 const NoChatSelected = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+      <div className="px-4 text-center sm:text-lg md:text-xl text-black font-semibold flex flex-col items-center gap-2">
         <p>Welcome to chatBot</p>
         <p>Select chat to get started</p>
         <TiMessages />
